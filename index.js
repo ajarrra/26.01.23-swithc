455fa4f3ec4cff9c824df323587ffdbe826b4ebc
Koshoeva Ajar <ajarka1334@gmail.com>
2023-01-26 15:10:51 +0100

    finished to class work primt

--- index.js
+++ index.js
@@ -27,6 +27,41 @@ myCity("Karakol");
 function myFriend(name){
     console.log("My friend is " + name);
 }
-myFriend ("Nestna");
+myFriend ("Nestna");//my friends is Nestana
 myFriend ("Aiday");
 myFriend("Anton");
+
+
+function printSom(a,b){//решение цифр 
+    let som = a + b;
+    console.log("The som of " + a + " and " + b + " is " + som);
+}
+
+printSom(5,10);// the som of 5 and 10 is 5
+printSom(-5, 0);
+printSom(100, 125);
+
+function printDiff(a, b){
+    let diff = a - b
+    console.log( "the diff of "+ a + " - " + b + " = " + diff );
+}
+
+printDiff(5,10); 
+
+
+function printMultiply (a,b){
+    let multiply = a * b
+    console.log("The multiply of " + a + " * " + b  + " = " + multiply);
+}
+printMultiply(10,10);
+
+function printDivide(a,b){
+    let divide = a / b
+    console.log("The multiply of " + a + " / " + b  + " = " + divide);
+}
+printDivide(5,10);
+
+
+
+
+
