e01c359cfb65a67d3d3e0574c52b86c666497dae
Koshoeva Ajar <ajarka1334@gmail.com>
2023-01-26 14:35:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//функции это набор инструкциий в котором рассписываем
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello(); // использовали эту функцию
+
+function sayBye (){
+    console.log("Bye");
+}
+sayBye();
